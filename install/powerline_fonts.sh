00a369f3ccfe784c2fea553c24f0eba4597e2210
Chinmay Garde <chinmaygarde@google.com>
2018-06-05 19:33:35 -0700

    Update powerline_fonts.sh

--- install/powerline_fonts.sh
+++ install/powerline_fonts.sh
@@ -12,6 +12,6 @@ trap NukeWorkspace EXIT
 echo Installing Powerline compatible fonts.
 mkdir -p ${WORKSPACE}
 cd ${WORKSPACE}
-git clone git@github.com:powerline/fonts.git powerline
+git clone https://github.com/powerline/fonts powerline
 cd powerline
 ./install.sh
